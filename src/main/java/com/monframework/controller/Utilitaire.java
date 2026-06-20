7dd8de6a4cfb91771a1173d0b96da8d63a2b4212
VlexandreMG <dodolashmou@gmail.com>
2026-06-20 22:41:01 +0300

    getFunctionsInClass

--- src/main/java/com/monframework/controller/Utilitaire.java
+++ src/main/java/com/monframework/controller/Utilitaire.java
@@ -1,30 +1,33 @@
 package com.monframework.controller;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Utilitaire {
     public static List<Class<?>> getClassesInPackage(String packageName) {
         List<Class<?>> classes = new ArrayList<>();
-        
+
         try {
-            // 1. Convertir le package (ex: "com.monapp") en chemin de dossier ("com/monapp")
+            // 1. Convertir le package (ex: "com.monapp") en chemin de dossier
+            // ("com/monapp")
             String packagePath = packageName.replace('.', '/');
-            
+
             // 2. Demander au ClassLoader de trouver l'URL de ce dossier
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
             URL packageUrl = classLoader.getResource(packagePath);
-            
+
             if (packageUrl == null) {
                 System.out.println("Le package " + packageName + " est introuvable.");
                 return classes;
             }
-            
+
             // 3. Convertir l'URL en fichier physique (Dossier)
             File directory = new File(packageUrl.getFile());
-            
+
             if (directory.exists() && directory.isDirectory()) {
                 // 4. Lister tous les fichiers du dossier
                 File[] files = directory.listFiles();
@@ -34,10 +37,10 @@ public class Utilitaire {
                         if (file.isFile() && file.getName().endsWith(".class")) {
                             // Enlever l'extension ".class" pour avoir juste le nom de la classe
                             String className = file.getName().substring(0, file.getName().length() - 6);
-                            
+
                             // Reconstituer le nom complet (ex: com.monapp.controller.EmpController)
                             String fullClassName = packageName + "." + className;
-                            
+
                             // Charger la classe en mémoire et l'ajouter à la liste
                             Class<?> clazz = Class.forName(fullClassName);
                             classes.add(clazz);
@@ -48,7 +51,7 @@ public class Utilitaire {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         return classes;
     }
 
@@ -62,5 +65,22 @@ public class Utilitaire {
         }
 
         return classesWithAnnotation;
+    }
+
+    public static List<Class<?>> getFunctionsInClass(Class<?> className) {
+        List<Method> toutesLesMethodes = new ArrayList<>();
+
+        // 1. On vérifie si la classe fournie n'est pas nulle pour éviter les plantages
+        if (className != null) {
+            // 2. Détecter toutes les méthodes publiques déclarées dans la classe
+            Method[] methodesArray = className.getDeclaredMethods();
+
+            // 3. Les mettre toutes dans notre liste
+            toutesLesMethodes.addAll(Arrays.asList(methodesArray));
+        } else {
+            System.out.println("La classe " + className + "n'existe pas.");
+        }
+
+        return toutesLesMethodes;
     }
 }
