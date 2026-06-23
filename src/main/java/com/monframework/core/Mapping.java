14fbe691e3f58226d991f5c9ef824740fb086172
VlexandreMG <dodolashmou@gmail.com>
2026-06-23 15:49:38 +0300

    Class Mapping

---
+++ src/main/java/com/monframework/core/Mapping.java
@@ -0,0 +1,24 @@
+package com.monframework.core;
+
+public class Mapping {
+    private Class className;
+    private Method methode;
+    
+    
+    public Mapping() {}
+    
+    public Class getClassName() {
+        return className;
+    }
+    public void setClassName(Class className) {
+        this.className = className;
+    }
+    public Method getMethode() {
+        return methode;
+    }
+    public void setMethode(Method methode) {
+        this.methode = methode;
+    }
+
+    
+}
