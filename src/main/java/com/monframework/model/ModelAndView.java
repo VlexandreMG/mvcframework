1edc4c54909ad0d94e67f4ef7642a552b051ed84
VlexandreMG <dodolashmou@gmail.com>
2026-07-14 16:05:12 +0300

    Modification de ModelAndView

--- src/main/java/com/monframework/model/ModelAndView.java
+++ src/main/java/com/monframework/model/ModelAndView.java
@@ -3,14 +3,22 @@ package com.monframework.model;
 import java.util.HashMap;
 
 public class ModelAndView {
+    
+    private String view;
     private HashMap<String, Object> data = new HashMap<>();
-
+    
     public void setAttribute(String key, Object value) {
         this.data.put(key,value);
     }
-
+    
     public HashMap<String, Object> getData() {
         return data;
     }
+    public String getView() {
+        return view;
+    }
 
+    public void setView(String view) {
+        this.view = view;
+    }
 }
